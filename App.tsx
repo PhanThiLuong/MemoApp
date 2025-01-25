14df60e28bbb7802ca44af2e7948d75a63ecfbf1
Phan Thi Luong <luongtit4101998@gmail.com>
2025-01-25 19:50:34 +0900

    Creat Hello component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-
+import Hello from './src/components/Hello';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>xin chào aaa
-      </Text>
+      <Hello/>
+      <Text>xin chào aaa</Text>
       <StatusBar style="auto" />
     </View>
   );
