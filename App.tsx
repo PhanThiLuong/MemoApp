86975897d47c32967f7ab74c9d3d550b9cb83c4d
Phan Thi Luong <luongtit4101998@gmail.com>
2025-01-25 15:21:02 +0900

    Install ESLint

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>xin chào</Text>
+      <Text>xin chào aaa
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
